6e59470b85c5ff6cbed3d553b1f90d2025b776a1
Michael Mussato <michimussato@gmail.com>
2026-04-11 13:30:23 +0200

    implements individual development/testing/debugging functionality

--- src/OpenStudioLandscapes/RustDeskServer/definitions.py
+++ src/OpenStudioLandscapes/RustDeskServer/definitions.py
@@ -4,16 +4,14 @@ from dagster import (
 )
 
 import OpenStudioLandscapes.RustDeskServer.assets
-from OpenStudioLandscapes.engine.features.upstream_asset_specs import assets_external
 
-assets = load_assets_from_modules(
+assets_base = load_assets_from_modules(
     modules=[OpenStudioLandscapes.RustDeskServer.assets],
 )
 
 
 defs = Definitions(
     assets=[
-        *assets,
-        *assets_external,
+        *assets_base,
     ],
 )
