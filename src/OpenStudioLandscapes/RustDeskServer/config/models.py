053652d0d779e9cb07bda640f719b36e35b0dd54
Michael Mussato <michimussato@gmail.com>
2026-04-30 22:37:00 +0200

    option to set logging level for custom loggers

--- src/OpenStudioLandscapes/RustDeskServer/config/models.py
+++ src/OpenStudioLandscapes/RustDeskServer/config/models.py
@@ -6,7 +6,10 @@ from pydantic import (
     Field,
 )
 
+from OpenStudioLandscapes.cli import LOGGING_LEVEL_DEFAULT
+
 LOGGER = get_dagster_logger(__name__)
+LOGGER.setLevel(LOGGING_LEVEL_DEFAULT)
 
 from OpenStudioLandscapes.engine.config.models import FeatureBaseModel
 
@@ -119,4 +122,10 @@ class Config(FeatureBaseModel):
         return ret
 
 
-CONFIG_STR = Config.get_docs()
+if __name__ == "__main__":
+    CONFIG_STR = Config.get_docs()
+else:
+    import yaml
+    CONFIG_STR = yaml.dump(
+        Config.model_json_schema(mode="serialization"),
+    )
